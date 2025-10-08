d5e71aa848ccb6096d45f97b7a942a859d8ce089
shupon0495 <shun2nakano057@gmail.com>
2025-10-09 00:58:03 +0900

    半二重通信ができないがそれ以外は完成

--- config.h
+++ config.h
@@ -1,12 +1,18 @@
 #pragma once
 
-#define SERIAL_USART_FULL_DUPLEX
-#define SERIAL_USART_TX_PIN GP0
-#define SERIAL_USART_RX_PIN GP1
+#define SERIAL_USART_HALF_DUPLEX
+#define SERIAL_USART_TX_PIN GP1
+#define SPLIT_HAND_PIN GP15
 #define DEBOUNCE 5
+#define SPLIT_POINTING_ENABLE /*スレーブ側からマスターにトラボを通信*/
+#define SPLIT_LED_STATE_ENABLE /*num lockなどを同期*/
+#define SPLIT_LAYER_STATE_ENABLE /*レイヤーを同期*/
+
 
 #define MATRIX_ROWS 10
 #define MATRIX_COLS 10
 
 #define ENCODER_RESOLUTION 4
+
+
 
